955c77b7e8664541fc0b894294929c04fe6f4426
heahgo <h34hg0@gmail.com>
2023-10-16 10:10:16 +0900

    Update selection_sort.cpp

--- cpp/sort/selection_sort.cpp
+++ cpp/sort/selection_sort.cpp
@@ -1,22 +1,45 @@
 #include <cstdio>
 
-void selectionSort(int *a, int size) {
-    int i, j, t, min, tmp;
+void Swap(int* a, int* b) {
+    if (a == b) return;
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
 
-    for (i = 0; i < size-1; i++) {
-        min = 1;
-        for (j = i+1; j < size; j++) {
-            if (a[j] < a[min]) min = j;
+int Selection(int *arr, int last_idx) {
+    int idx = 0, tmp = arr[0];
+    for (int i = 1; i <= last_idx; i++) {
+        if (tmp < arr[i]) {
+            idx = i;
+            tmp = arr[i];
         }
-        tmp = a[i];
-        a[i] = a[min];
-        a[min] = tmp;
-        printf("\n%d단계 : ", i+1);
-        for (t = 0; t < size; t++) printf("%3d ", a[t]);
+    }
+    return idx;
+} 
+
+void SelectionSort(int *arr, int size) {
+    int idx = 0, last_idx = size-1;
+
+    for (int i = 0; i < size; i++) {
+        idx = Selection(arr, last_idx);
+        Swap(&arr[idx], &arr[last_idx]);
+        --last_idx;
+    }   
+}
+
+void PrintArr(int* arr, int size) {
+    printf("Arr : [");
+    for (int i = 0; i < size; i++) {
+        if (i == size-1) printf("%d]\n", arr[i]);
+        else printf("%d, ", arr[i]);
     }
 }
 
 int main() {
     int arr[] = {123, 1245, 50, 38, 177, 463, 33};
-    selectionSort(arr, 7);
+    SelectionSort(arr, 7);
+    PrintArr(arr, 7);
+    // Swap(&arr[0], &arr[6]);
+    // PrintArr(arr, 7);
 }
