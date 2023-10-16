f70b78a66ad46c60a95c358d521dd69fb744e123
heahgo <h34hg0@gmail.com>
2023-10-16 10:44:31 +0900

    Remove Description

--- cpp/sort/selection_sort.cpp
+++ cpp/sort/selection_sort.cpp
@@ -7,6 +7,14 @@ void Swap(int* a, int* b) {
     *a ^= *b;
 }
 
+void PrintArr(int* arr, int size) {
+    printf("Arr : [");
+    for (int i = 0; i < size; i++) {
+        if (i == size-1) printf("%d]\n", arr[i]);
+        else printf("%d, ", arr[i]);
+    }
+}
+
 int Selection(int *arr, int last_idx) {
     int idx = 0, tmp = arr[0];
     for (int i = 1; i <= last_idx; i++) {
@@ -28,18 +36,8 @@ void SelectionSort(int *arr, int size) {
     }   
 }
 
-void PrintArr(int* arr, int size) {
-    printf("Arr : [");
-    for (int i = 0; i < size; i++) {
-        if (i == size-1) printf("%d]\n", arr[i]);
-        else printf("%d, ", arr[i]);
-    }
-}
-
 int main() {
     int arr[] = {123, 1245, 50, 38, 177, 463, 33};
     SelectionSort(arr, 7);
     PrintArr(arr, 7);
-    // Swap(&arr[0], &arr[6]);
-    // PrintArr(arr, 7);
 }
