f0b8a092fbfa9de8516918720d41ac6f51a91a7b
heahgo <h34hg0@gmail.com>
2023-10-18 22:55:54 +0900

    Update cpp/sort/quick_sort.cpp (Implementation right pivot quick sort)

--- cpp/sort/quick_sort.cpp
+++ cpp/sort/quick_sort.cpp
@@ -1,5 +1,12 @@
 #include <cstdio>
 
+void Swap(int* a, int* b) {
+    if (a == b) return;
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
 void PrintArr(int* arr, int size) {
     printf("Arr : [");
     for (int i = 0; i < size; i++) {
@@ -8,10 +15,26 @@ void PrintArr(int* arr, int size) {
     }
 }
 
-void QuickSort();
+int RightPartition(int* arr, int left, int right) {
+    int pivot = arr[right];
+    int L = left - 1;
+    for (int i = left; i < right; i++) {
+        if (arr[i] <= pivot) Swap(&arr[++L], &arr[i]);
+    }
+    Swap(&arr[L+1], &arr[right]);
+    return L+1;
+} 
+
+void RightQuickSort(int* arr, int left, int right) {
+    if (left < right) {
+        int mid = RightPartition(arr, left, right);
+        RightQuickSort(arr, left, mid-1);
+        RightQuickSort(arr, mid+1, right);
+    }
+}
 
 int main() {
-    int arr[] = {123, 1245, 50, 38, 177, 463, 33};
-    QuickSort();
-    PrintArr(arr, 7);
+    int arr1[] = {123, 1245, 50, 38, 177, 463, 33};
+    RightQuickSort(arr1, 0, 6);
+    PrintArr(arr1, 7);
 }
