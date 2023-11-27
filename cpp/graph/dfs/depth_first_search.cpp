7cc0848805d52df1a056cf2502f40a12cf2f29c3
heahgo <h34hg0@gmail.com>
2023-11-27 14:29:39 +0900

    Update dfs

--- cpp/graph/dfs/depth_first_search.cpp
+++ cpp/graph/dfs/depth_first_search.cpp
@@ -32,7 +32,9 @@ class Graph {
         uint32_t vertex_num_;
         uint32_t edge_num_;
         
-        void Print() {  // Graph print Using Adjacent Links List
+        void Print() {  // Graph print Using Adjacent Linked List
+        printf("**** Linked List Graph ****\n");
+        printf("--------+------------------\n");
             for (uint32_t i = 0; i < vertex_num_; i++) {
                 Vertex* tmp = graph_.at(i);
                 printf("idx : %u | ", i+1);
@@ -43,23 +45,22 @@ class Graph {
                 }
                 printf("%u \n", tmp->num_);
             }
+        printf("--------+------------------\n");
         }
 
         Graph(uint32_t vertex_num, uint32_t edge_num) : vertex_num_(vertex_num), edge_num_(edge_num) {
             for (uint32_t i = 0; i < vertex_num; i++) {
-                Vertex* tmp = new Vertex();
+                Vertex* tmp = new Vertex(i+1);
                 graph_.push_back(tmp);
             }
             uint32_t v1, v2;
             for (uint32_t i = 0; i < edge_num; i++) {
                 scanf("%u %u", &v1, &v2); // Input two-way edges
                 Vertex* tmp1 = new Vertex(v2);
-                graph_.at(v1-1)->num_ += 1;
                 graph_.at(v1-1)->Add(tmp1);  
 
                 if (v1 == v2) continue;
                 Vertex* tmp2 = new Vertex(v1);
-                graph_.at(v2-1)->num_ += 1;
                 graph_.at(v2-1)->Add(tmp2);
             }
         }
@@ -81,12 +82,12 @@ vector<uint32_t> DFS(Graph* graph, uint32_t start) {
 
     uint32_t n = start-1;
     Vertex* v = g.at(n);
-    
+
     stack.push_back(v);
     visited[n] = true;
     result.push_back(start);
-    // printf("%u ", start); //debug
     v = v->Next();
+
     while(stack.size() != 0) {
         if (v != NULL) {
             if (visited[v->num_-1] == true) {
@@ -94,20 +95,15 @@ vector<uint32_t> DFS(Graph* graph, uint32_t start) {
                 continue;
             }
             n = v->num_-1;
-            // printf("PUSH %u\n", n+1); // print push stack
-            // printf("%u ", n+1);
             result.push_back(n+1);
             visited[n] = true;
             stack.push_back(v);
             v = g.at(n);
         } else {
             v = stack.back();
-            // if (stack.size() == 1) printf("POP %u\n", start); // print pop stack
-            // else printf("POP %u\n", v->num_);
             stack.pop_back();
         }
     }
-    printf("\n");
     delete[] visited;
     return result;
 }
@@ -117,14 +113,12 @@ int main() {
     scanf("%u %u", &vertex, &edge); 
     Graph graph = Graph(vertex, edge);
 
-    printf("---- Linked List Graph ----\n");
     graph.Print();
-    printf("---------------------------\n");
-    printf("\n");
-
     vector<uint32_t> dfs_result;
-    printf("DFS : ");
+
+    printf("\nDFS : ");
     dfs_result = DFS(&graph, 1);
+
     for (uint32_t i = 0; i < dfs_result.size(); i++) {
         printf("%u ", dfs_result.at(i));
     }
